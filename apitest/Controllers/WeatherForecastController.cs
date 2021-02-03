11ef641f64308e7e4c260bcd634a777323282a89
SeanDunlop <30559729+SeanDunlop@users.noreply.github.com>
2021-02-03 17:52:28 -0500

    init commit

--- apitest/Controllers/WeatherForecastController.cs
+++ apitest/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace apitest.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cooler", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
