e1561f549c85ca245276876a3746bf18e11545ec
Aevo55 <dawspuddingcat@gmail.com>
2021-04-04 22:54:30 -0400

    ahhhhh

---
+++ apitest/Models/Credentials.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apitest.Models
+{
+    public class Credentials
+    {
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ apitest/Models/Sessions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apitest.Models
+{
+    public class Sessions
+    {
+        
+        public int SessionID { get; set; }
+
+        public String SessionGuid { get; set; }
+        public DateTime Timeout { get; set; }
+    }
+}
