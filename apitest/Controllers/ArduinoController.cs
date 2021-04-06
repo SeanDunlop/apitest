2c0c2b3814e358ad588e783ecdb4bf953c6c441b
Aevo55 <dawspuddingcat@gmail.com>
2021-04-05 21:12:58 -0400

    ahh help

--- apitest/Controllers/ArduinoController.cs
+++ apitest/Controllers/ArduinoController.cs
@@ -60,7 +60,5 @@ namespace apitest.Controllers
             }
         }
 
-        [HttpGet]
-        public 
     }
 }
--- apitest/Controllers/LoginController.cs
+++ apitest/Controllers/LoginController.cs
@@ -14,6 +14,7 @@ using apitest.Common;
 namespace apitest.Controllers
 {
     [Route("api/Login")]
+    [ApiController]
     public class LoginController : ControllerBase
     {
         private readonly ScheduleContext _context;
